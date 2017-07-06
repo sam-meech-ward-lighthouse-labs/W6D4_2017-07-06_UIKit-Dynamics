1efd077a6576d4f7aa4e37bb31c30b8e632dd4ae
Sam Meech-Ward <sam@meech-ward.com>
2017-07-06 09:14:58 -0700

    Initial Commit

---
+++ DynamicsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicsDemo
+//
+//  Created by Sam Meech-Ward on 2017-07-06.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
